af857352dc58818f44aa014ee2629a24ce014819
Jomar HA Nguyen <jomarnguyen@Jomars-MBP.hsd1.ga.comcast.net>
2020-09-30 11:21:36 -0400

    DONE auto slide

--- src/Home/Slide/Slide.js
+++ src/Home/Slide/Slide.js
@@ -5,7 +5,7 @@ function Slide() {
   useEffect(() => {
     const autoSlide = () => {
       const list = document.querySelectorAll(".slide ul li");
-      setInterval(() => {
+      setInterval(function () {
         var currentIndex = 0;
         var currentSlide = document.querySelector(".view");
         for (
@@ -13,7 +13,15 @@ function Slide() {
           (currentSlide = currentSlide.previousElementSibling);
           currentIndex++
         ) {}
-      }, 3000);
+        for (let i = 0; i < list.length; i++) {
+          list[i].classList.remove("view");
+        }
+        if (currentIndex < list.length - 1) {
+          list[currentIndex].nextElementSibling.classList.add("view");
+        } else {
+          list[0].classList.add("view");
+        }
+      }, 5000);
     };
     autoSlide();
   });
@@ -25,27 +33,30 @@ function Slide() {
             src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MmUwZWEyOWUt/MmUwZWEyOWUt-OTM5MWYyNDgt-w3000._CB404706413_.jpg"
             alt=""
           />
+          <div className="slide__fade"></div>
         </li>
         <li className="view">
           <img
             src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/education/merch/gw/DER_BestSellersReading_DesktopHero_3000x1200_EN._CB404812456_.jpg"
             alt=""
           />
+          <div className="slide__fade"></div>
         </li>
         <li>
           <img
             src="https://images-na.ssl-images-amazon.com/images/G/01/IMDbTV/gateway/twilight/GW_DHERO_Twilight_3000x1200_EN-US_8546_v12x._CB404978515_.jpg"
             alt=""
           />
+          <div className="slide__fade"></div>
         </li>
         <li>
           <img
             src="https://images-na.ssl-images-amazon.com/images/G/01/img18/home/journeys/MTIzMTg1NGEt/MTIzMTg1NGEt-NDM2ZjFjNDEt-w3000._CB404804088_.jpg"
             alt=""
           />
+          <div className="slide__fade"></div>
         </li>
       </ul>
-      <div className="slide__fade"></div>
     </div>
   );
 }
