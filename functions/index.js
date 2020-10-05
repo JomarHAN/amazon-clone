abf1383ef0fc690c3b274ae2caaf7e6a0892d70a
Jomar HA Nguyen <jomarnguyen@Jomars-MBP.hsd1.ga.comcast.net>
2020-10-05 17:14:48 -0400

    NEXT TODO: cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,4 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express = require('express')
+const cors = require('cors')
+const stripe = require('stripe')('sk_test_51HYyKtG3acrIAEXlEUlO57clRSnXFeTE3IdB0XZEdRZHiNJWhGfq8lxuocofuMjbsBTSmyFVUAiwlWOc2vb34NH700Quwl2A9j')
